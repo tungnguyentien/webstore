7aa782a63738d5cf0f140ec6a485f6b81260e67c
Nguyen Tien Tung <tungnguyentien.cs@gmail.com>
2015-10-07 23:34:08 +0700

    Add layout cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // Empty JS for your own code to be here
 $(document).ready(function() {
+	// Search
 	$('#search_query_top').on('click',function(){
 		$(this).attr('value','');
 		$('.button-search').addClass('active');
@@ -8,5 +9,23 @@ $(document).ready(function() {
 	$('#search_query_top').on('focusout',function(){
 		$(this).attr('value','Search...');
 		$('.button-search').removeClass('active');
-	})
+	});
+
+	// Add item to cart
+	$('.add-to-cart').click(function(){
+		$('.backdrop').fadeIn(250);
+		$('.layout-cart').fadeIn(450);
+		var img = $(this).parent().siblings('.container').find('img');
+		var src = img.attr('src');
+		console.log(img.attr('src'));
+		$('.image-view img').attr('src',src);
+	});
+	$('.backdrop').click(function(){
+		$('.layout-cart').fadeOut(300);
+		$(this).fadeOut(300);
+	});
+	$('.icon-remove').click(function(){
+		$('.layout-cart').fadeOut(300);
+		$('.backdrop').fadeOut(300);
+	});
 });
